d1a018fb0cd905880e81474ca6d6490d9c4627f2
marm00 <markscheperman@gmail.com>
2026-01-09 21:00:42 +0100

    compile & link with .ico scaffold

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 @echo off
-clang cinema.c libsais.c -std=c11 -O3 -DNDEBUG -fopenmp -DCIN_OPENMP -DLIBSAIS_OPENMP -flto=thin -fuse-ld=lld-link -o cinema.exe
+llvm-rc cinema.rc -fo cinema.res
+clang cinema.c libsais.c cinema.res -std=c11 -O3 -DNDEBUG -fopenmp -DCIN_OPENMP -DLIBSAIS_OPENMP -flto=thin -fuse-ld=lld-link -o cinema.exe
