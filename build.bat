b54f995fa0a5537eb5c0ef82e98a4238153b6773
marm00 <markscheperman@gmail.com>
2026-01-17 17:25:15 +0100

    debug release parallel build options

--- build.bat
+++ build.bat
@@ -1,3 +1,24 @@
 @echo off
+
+for %%a in (%*) do set "%%~a=1"
+
+if not "%release%"=="1" set debug=1
+if "%debug%"=="1" set release=0
+if "%release%"=="1" set debug=0
+
+if not "%nomp%"=="1" set parallel=1
+if "%nomp%"=="1" set parallel=0
+if "%parallel%"=="1" echo [openmp on] && set omp= -fopenmp -DCIN_OPENMP -DLIBSAIS_OPENMP
+if "%parallel%"=="0" echo [openmp off] && set omp=
+
+set warn= -Wall -Wextra -Wpedantic -Wstrict-prototypes -Wmissing-prototypes -Wconversion -Wsign-conversion -Wshadow -Wformat=2 -Wno-unused-function
+
 llvm-rc cinema.rc -fo cinema.res
-clang cinema.c libsais.c cinema.res -std=c11 -O3 -DNDEBUG -fopenmp -DCIN_OPENMP -DLIBSAIS_OPENMP -flto=thin -fuse-ld=lld-link -o cinema.exe
+
+if "%release%"=="1" (
+    echo [release build]
+    clang cinema.c libsais.c cinema.res -std=c11 -O2 -DNDEBUG %omp% -flto=thin -fuse-ld=lld-link -o cinema.exe
+) else (
+    echo [debug build]
+    clang cinema.c libsais.c cinema.res -std=c11 -g -gcodeview %omp% %warn% -fuse-ld=lld-link -Wl,/DEBUG -Wl,/PDB:cinema.pdb -o cinema.exe
+)
